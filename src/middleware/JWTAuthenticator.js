7ac6556a0b4be4f6f074424b39e4558f61e10e24
George Benos <gbenos@telesto.gr>
2022-11-24 13:16:11 +0200

    more jwt logging

--- src/middleware/JWTAuthenticator.js
+++ src/middleware/JWTAuthenticator.js
@@ -3,14 +3,16 @@ import * as jose from 'jose'
 
 dotenv.config()
 export default function authenticateJWT(req, res, next){
-
+    
+    console.log("Retrieving JWKS from "+process.env.OIDC_URL + process.env.JWKS_PATH);
     // @ts-ignore
     const JWKS = jose.createRemoteJWKSet(new URL(process.env.OIDC_URL + process.env.JWKS_PATH))
+    console.log("retrieved JWKS as "+JWKS)
 
     const id_token = req.headers.id_token
     const access_token = req.headers.access_token
     if (id_token) {
-        console.log("verifying id token...")
+        console.log("verifying id token "+req.headers.id_token)
         jose.jwtVerify(id_token, JWKS).then( result => {
             req.id_token = result.payload
             req.raw_id_token = id_token
@@ -18,8 +20,8 @@ export default function authenticateJWT(req, res, next){
             console.log(`Token verified as: ${JSON.stringify(req.id_token)}`)
             next();
         }).catch( err=>{
-            console.log("error: "+err.message)
-            return res.status(403).json({error : err.message})
+            console.log("JWTerror: "+err.message)
+            return res.status(403).json({JWTerror : err.message})
         });
     } else {
         return res.status(401).json({error: "No logged in user"})
